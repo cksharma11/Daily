7d897807dfdf96d0c10e05d3c0d54c062c631ef3
Chandan Kumar <cksharma122@gmail.com>
2020-04-17 23:24:48 +0530

    Add update api

--- main.go
+++ main.go
@@ -60,6 +60,27 @@ func getAllEvent(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(events)
 }
 
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	var event event
+	eventID := mux.Vars(r)["id"]
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Enter correct event structure")
+	}
+
+	json.Unmarshal(reqBody, &event)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			singleEvent.Title = event.Title
+			singleEvent.Description = event.Description
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello API")
 }
@@ -71,6 +92,7 @@ func main() {
 	router.HandleFunc("/event", createEvent).Methods("POST")
 	router.HandleFunc("/event/{id}", getOneEvent).Methods("GET")
 	router.HandleFunc("/events", getAllEvent).Methods("GET")
+	router.HandleFunc("/event/{id}", updateEvent).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
