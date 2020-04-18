054caa4bf86bf6f3ee03c85a1b8a01ec48cf9709
Chandan Kumar <cksharma122@gmail.com>
2020-04-18 15:31:49 +0530

    Add done field to types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -4,6 +4,7 @@ type Task struct {
 	ID          string `json:"id"`
 	Date        string `json:"date"`
 	Description string `json:"description"`
+	Done        bool   `json:"done"`
 }
 
 type AllTasks []Task
@@ -13,10 +14,12 @@ var Tasks = AllTasks{
 		ID:          "1",
 		Date:        "2020-04-18T09:10:31.733Z",
 		Description: "Task 1",
+		Done:        false,
 	},
 	{
 		ID:          "2",
 		Date:        "2020-04-18T09:10:31.733Z",
 		Description: "Task 2",
+		Done:        false,
 	},
 }
