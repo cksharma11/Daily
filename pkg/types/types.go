8bc71e21ec7b690f8570fbb8737909fbc9beb307
Chandan Kumar <cksharma122@gmail.com>
2020-04-18 17:04:36 +0530

    Add time type in Date field

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,10 +1,12 @@
 package types
 
+import "time"
+
 type Task struct {
 	ID          string `json:"id"`
-	Date        string `json:"date"`
 	Description string `json:"description"`
 	Done        bool   `json:"done"`
+	Date        time.Time
 }
 
 type AllTasks []Task
@@ -12,13 +14,13 @@ type AllTasks []Task
 var Tasks = AllTasks{
 	{
 		ID:          "1",
-		Date:        "2020-04-18T09:10:31.733Z",
+		Date:        time.Now(),
 		Description: "Task 1",
 		Done:        false,
 	},
 	{
 		ID:          "2",
-		Date:        "2020-04-18T09:10:31.733Z",
+		Date:        time.Now(),
 		Description: "Task 2",
 		Done:        false,
 	},
